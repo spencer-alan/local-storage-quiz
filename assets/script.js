3417651db4761c030049db034d849dc41f01bdbb
b1acksmyth <smith.spence13@gmail.com>
2020-07-06 20:06:26 -0700

    wokred out a new array and cleaned up buttons

--- assets/script.js
+++ assets/script.js
@@ -6,87 +6,90 @@ let submit = document.querySelector('#save-score');
 let remove = document.querySelector('#delete-score');
 let initial = document.querySelector('#initial');
 
-let questions = [
-  { 
-    question: 'The "=" in JavaScript means __________.',
-    a: 'equals',
-    b: 'assignment',
-    c: 'same as',
-    d: 'agree',
-    answer: 'b',
-  },
-  {
-    question: 'jQuery is a simplified version of __________.',
-    a: 'JavaScript',
-    b: 'HTML',
-    c: 'CSS',
-    d: 'React',
-    answer: 'a',
-  },
-  {
-    question: 'An HTML tag is surronded by __________.',
-    a: '[]',
-    b: '()',
-    c: '<>',
-    d: '{}',
-    answer: 'c',
-  },
-  {
-    question: 'The link to external CSS files in HTML goes inside the __________ tag. ',
-    a: '<body>',
-    b: '<section>',
-    c: '<head>',
-    d: '<div>',
-    answer: 'c',
-  },
-  {
-    question: 'Which header element has the smallest font by default?.',
-    a: '<h1>',
-    b: '<h2>',
-    c: '<h3>',
-    d: '<h4>',
-    answer: 'd',
-  }
-];
-let position = 0;
+let questions = [question1, question2, question3, question4, question5]
+let qustion1 = {
+  question: 'The link to external CSS files in HTML goes inside the __________ tag. ',
+  a: '<body>',
+  b: '<section>',
+  c: '<head>',
+  d: '<div>',
+  answer: 'c',
+};  
+let question2 = { 
+  question: 'The "=" in JavaScript means __________.',
+  a: 'equals',
+  b: 'assignment',
+  c: 'same as',
+  d: 'agree',
+  answer: 'b',
+};
+let question3 = {
+  question: 'jQuery is a simplified version of __________.',
+  a: 'JavaScript',
+  b: 'HTML',
+  c: 'CSS',
+  d: 'React',
+  answer: 'a',
+};
+let question4 = {
+  question: 'An HTML tag is surronded by __________.',
+  a: '[]',
+  b: '()',
+  c: '<>',
+  d: '{}',
+  answer: 'c',
+};
+let question5 = {
+  question: 'Which header element has the smallest font by default?.',
+  a: '<h1>',
+  b: '<h2>',
+  c: '<h3>',
+  d: '<h4>',
+  answer: 'd',
+};
+let i = 0;
 let timer = document.querySelector('#timer');
 let time = 10;
 let timeScore = time.value;
 
-// function quizStart(){
-//   // this starts the timer
-//   let countdown = setInterval(function(){
-//     timer.innerHTML = time;
-//     time--;
-//     if (time < 0){
-//       clearInterval(countdown);
-//     }
-//   }, 1000);
-// };
-start.addEventListener('click', renderQuestions());
+
+
+  function startTimer(){
+  // this starts the timer
+  let countdown = setInterval(function(){
+    timer.innerHTML = time;
+    time--;
+    if (time < 0){
+      clearInterval(countdown);
+    }
+  }, 1000);
+}
+
 
 // renders the questions on the page 
 function renderQuestions(){
-  if (position > questions.length){
+  if (i > questions.length){
     quiz.innerHTML = `Quiz Finished! Your score is ${time}.`;
     if (endForm.style.display === 'none'){
       endForm.style.display = 'block';
     } else {
       endForm.style.display = 'none';
     }
-    quiz.innerHTML = questions[position].question;
-    button1 = document.createElement('<button>');
-    button1.textContent = questions[position].a;
-    quiz.append(button1);
-    button2 = document.createElement('<button>');
-    button2.textContent = questions[position].b;
-    quiz.append(button2);
-    button3 = document.createElement('<button>');
-    button3.textContent = questions[position].c;
-    quiz.append(button3);
-    button4 = document.createElement('<button>');
-    button4.textContent = questions[position].d;
-    quiz.append(button4);
+      for (let i = 0; i < question.length; i++){
+      quiz.innerHTML = questions[i].question;
+      button1 = document.createElement('<button>');
+      button1.textContent = questions[i].a;
+      quiz.append(button1);
+      button2 = document.createElement('<button>');
+      button2.textContent = questions[i].b;
+      quiz.append(button2);
+      button3 = document.createElement('<button>');
+      button3.textContent = questions[i].c;
+      quiz.append(button3);
+      button4 = document.createElement('<button>');
+      button4.textContent = questions[i].d;
+      quiz.append(button4);
+    }
   }
 }
   // function checkAnswer(){
@@ -108,17 +111,16 @@ function renderQuestions(){
   //   renderQuestion();
   
 
-// // This will start the timer
-// start.addEventListener('click', timeCountdown);
-// // This will start to render questions
-// start.addEventListener('click', renderQuestions);
-
+// This will start the timer
+start.addEventListener('click', startTimer);
+// This will start to render questions
+start.addEventListener('click', renderQuestions);
 // This should store the initials and the time when the quiz stopped
 submit.addEventListener('click', function(event) {
   localStorage.setItem('initials', initial);
   localStorage.setItem('score', timeScore);
 });
-
+// this should clear local storage
 remove.addEventListener('click', function(event) {
   localStorage.clear();
 });
